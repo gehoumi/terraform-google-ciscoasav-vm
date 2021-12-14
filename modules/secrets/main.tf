cc24ba44dcbb7259503128d80710b8fa3fc1ff01
gehoumi <>
2021-12-14 17:06:40 -0500

    add option to create the secret for all resources in secret

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,10 +1,14 @@
 resource "random_password" "password" {
+  count = var.create_secret ? 1 : 0
+
   length           = 20
   special          = true
   override_special = "$#@"
 }
 
 resource "google_secret_manager_secret" "secret_basic" {
+  count = var.create_secret ? 1 : 0
+
   project   = var.project_id
   secret_id = var.secret_id
 
@@ -27,6 +31,6 @@ resource "google_secret_manager_secret" "secret_basic" {
 resource "google_secret_manager_secret_version" "secret_version_basic" {
   count = var.create_secret ? 1 : 0
 
-  secret      = google_secret_manager_secret.secret_basic.id
-  secret_data = random_password.password.result
+  secret      = google_secret_manager_secret.secret_basic[0].id
+  secret_data = random_password.password[0].result
 }
